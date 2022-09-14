935145fad8c33adaf96a40db23a2f86e03190f3d
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-09-14 12:28:56 -0300

    Adicionando bibliotecas do google fonts e contexto de tela

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+//import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import{ StatusBar} from 'react-native';
+import { Background } from './src/components/Background/index'; 
+
+import {
+   useFonts,
+   Inter_400Regular,
+   Inter_600SemiBold,
+   Inter_700Bold,
+   Inter_900Black,
+} from '@expo-google-fonts/inter';
+
+
+
+
+// LINK DO FIGMA:
+// https://www.figma.com/community/file/1150897317533332617
+
 
 export default function App() {
+
+  // obtendo variavel q ira dizer se as fontes foram carregadas ou nao
+  const [fontsLoaded] = useFonts({
+      Inter_400Regular,
+      Inter_600SemiBold,
+      Inter_700Bold,
+      Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Background> 
+          <StatusBar 
+              barStyle="light-content"
+              backgroundColor="transparent"
+              translucent
+              >
+          </StatusBar>
+
+          { fontsLoaded ? Status }
+      </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
